8d847012266d058777186c490e86d2f9c43243e9
Chaya Deaver <chayadeaver@gmail.com>
2019-09-15 19:32:48 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  holiday_hash.each {|season| season.to_s}
+  holiday_hash.each {|season| puts season.to_s}
 end
 
 def all_holidays_with_bbq(holiday_hash)

eca19b51b6dc93c3bc8ccd5c5309612ffa860b88
Chaya Deaver <chayadeaver@gmail.com>
2019-09-15 20:38:31 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,5 +70,11 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  holiday_hash.each do |season, holiday|
+    holiday.collect do |days, supplies|
+      if supplies.include?("BBQ")
+        holiday_hash[days]
+      end
+    end
+  end
 end

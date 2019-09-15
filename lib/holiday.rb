82fa12af9a1dc5aa0089465e740bfffd17eeebff
Chaya Deaver <chayadeaver@gmail.com>
2019-09-15 18:22:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,7 +27,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   holiday_hash.each do |season, holiday|
     if season == :winter
-      holiday.each do |activity, supply|
+      holiday.each do |activity|
         activity << supply
       end
     end
